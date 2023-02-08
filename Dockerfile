fd7323aa2a1f1fa29320cb0d5ff61860e2da060b
DanBrown47 <danwand47@gmail.com>
2023-02-08 11:59:20 +0530

    Add : Users and Companies

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Python image as the base image
+FROM python:3.9-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements.txt file to the container
+COPY requirements.txt ./
+
+# Install the required packages using pip
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the application files to the container
+COPY . ./
+
+# Expose the default Flask port
+EXPOSE 5000
+
+# Define the command to run the Flask application
+CMD ["python3", "app.py","host=0.0.0.0","port=5000"]
--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 
 from flask import Flask, request, Response, jsonify
-import flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-# from flask_cors import CORS
-
-
+from sqlalchemy.exc import IntegrityError
+from marshmallow import Schema, fields
 
 app = Flask(__name__)
 # CORS(app)
@@ -21,28 +19,45 @@ class User(db.Model):
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
 class Company(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(20), nullable=False, unique=True)
     ceo = db.Column(db.String(80), nullable=False)
     cto = db.Column(db.String(80), nullable=False)
     address = db.Column(db.String(80), nullable=False)
-    phone = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80), nullable=False)
     website = db.Column(db.String(80), nullable=False)
     following = db.Column(db.Integer(), nullable=False)
     donation = db.Column(db.Integer(), nullable=False)
+
+class CompanySchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    ceo = fields.Str(required=True)
+    cto = fields.Str(required=True)
+    address = fields.Str(required=True)
+    email = fields.Str(required=True)
+    website = fields.Str(required=True)
+    following = fields.Int(required=True)
+    donation = fields.Int(required=True)
+
+company_schema = CompanySchema()
+company_schemas = CompanySchema(many=True)
+
 
 with app.app_context():
     print("Pre flight check  " + str(app.name))
     db.create_all()    
     print("Database Initialized")
 
-@app.route('/get_all_company', methods=['GET'])
-def get_all_company():
-    all_company = Company.query.all()
-    result = company_schema.dump(all_company)
-    return jsonify(result)
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -52,24 +67,63 @@ def register():
     print(password)
     hashed_password = bcrypt.generate_password_hash(password)
     new_user = User(username=username, password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({'message': 'User added successfully'})
+    try:    
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'User added successfully'}),200
+    except IntegrityError:
+        return jsonify({'message': 'User already exists'}),403
+
+@app.route('/add_company', methods=['POST'])
+def add_company():
+    
+    data = request.get_json()
+    admin_token = data.get('admin_token')
+    name = data.get('name')
+    ceo = data.get('ceo')
+    cto = data.get('cto')
+    address = data.get('address')
+    email = data.get('email')
+    website = data.get('website')
+    following = data.get('following')
+    donation = data.get('donation')
+    new_company = Company(name=name, ceo=ceo, cto=cto, address=address, email=email, website=website, following=following, donation=donation)
+    try:
+        db.session.add(new_company)
+        db.session.commit()
+        return jsonify({'message': 'Company added successfully'}),200
+    except IntegrityError as e:
+        print(e)
+        return jsonify({'message': 'Company is already there'}),403
+    
+
+
+@app.route('/get_company/<id>', methods=['GET'])
+def get_company(id):
+    company = Company.query.get(id)
+    result =  company_schema.dump(company)
+    return jsonify(result)
+
+@app.route('/get_all_company', methods=['GET'])
+def get_all_company():
+    all_company = Company.query.all()
+    result = company_schemas.dump(all_company)
+    return jsonify(result)
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-    user = user.query.filter_by(username=username).first()
+    user = User.query.filter_by(username=username).first()
 
     if user:
         if bcrypt.check_password_hash(user.password, password):
-            return jsonify({'message': 'logged in', 'id': user.id, 'username': user.username})
+            return jsonify({'message': 'logged in', 'id': user.id, 'username': user.username}),200
         else:
-            return jsonify({'message': 'wrong password'})
+            return jsonify({'message': 'wrong password'}),403
     else:
-        return jsonify({'message': 'user not found'})
+        return jsonify({'message': 'user not found'}),404
 
 if __name__=="__main__":
     app.run(debug=True)
